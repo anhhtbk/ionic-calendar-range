f09de20aef91b9606116e53181407aa735f3bd8c
anhhtbk <anhhtbk@gmail.com>
2023-08-23 21:42:15 +0700

    get selected date

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { IonButton, IonDatetime, IonicModule, IonModal } from '@ionic/angular';
 
 @Component({
     selector: 'app-home',
@@ -11,22 +11,27 @@ import { IonicModule } from '@ionic/angular';
     imports: [CommonModule, FormsModule, IonicModule],
 })
 export class HomePage {
-    @ViewChild('datetime') datetime!: ElementRef;
+    @ViewChild('datetime') datetime!: IonDatetime;
+    @ViewChild('modal') modal!: IonModal;
     constructor() {
-
     }
 
-    myDates: string[] = [new Date().toISOString().substring(0,10)];
+    selectedDate: string[] = [];
+    myDates: string[] = [new Date().toISOString().substring(0, 10)];
     highlightedDates: any[] = [];
+    selectedText = "Select date";
 
     dateChanged() {
-        console.log(this.myDates);
-
         if (this.myDates.length == 3) {
             this.myDates = [this.myDates[2]]
         }
 
+        this.updateHighlightedDates();
+    }
+
+    updateHighlightedDates() {
         var dates = this.myDates.map(date => new Date(date));
+        this.highlightedDates = [];
 
         if (dates.length == 2) {
             var firstDate, lastDate;
@@ -38,32 +43,53 @@ export class HomePage {
                 firstDate = dates[1];
                 lastDate = dates[0];
             }
-            this.highlightedDates = [];
+
             for (var date = firstDate; date <= lastDate; date.setDate(date.getDate() + 1)) {
                 this.highlightedDates.push({
-                    date: date.toISOString().substring(0,10),
+                    date: date.toISOString().substring(0, 10),
                     textColor: 'var(--ion-color-secondary-contrast)',
                     backgroundColor: '#3880ff'
                 })
 
             }
-            console.log(this.highlightedDates);
         }
-        else {
-            this.highlightedDates = [];
-        }
+    }
+
+    openDatePicker() {
+        this.modal.present();
     }
 
     reset() {
-        // this.datetime!.reset();
-        this.highlightedDates = [];
+        if (this.selectedDate.length > 0) {
+            this.myDates = this.selectedDate;
+        }
+        else {
+            this.myDates = [new Date().toISOString().substring(0, 10)];
+        }
+        this.updateHighlightedDates();
     }
 
     cancel() {
-        // this.datetime!.cancel();
+        this.modal.dismiss();
+        this.reset();
     }
 
     confirm() {
-        // this.datetime!.confirm();
+        this.modal.dismiss();
+        this.selectedDate = this.myDates;
+        var length = this.selectedDate.length;
+        switch (length) {
+            case 0:
+                this.selectedText = "Select date";
+                break;
+            case 1:
+                this.selectedText = "Select date (" + this.selectedDate[0] + ")";
+                break;
+            case 2:
+                this.selectedText = "Select date (" + this.selectedDate[0] + " - " + this.selectedDate[1] + ")";
+                break;
+            default:
+                break;
+        }
     }
 }
